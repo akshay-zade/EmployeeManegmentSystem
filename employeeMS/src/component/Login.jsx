bd54ffc121fcc26c47ffacb03e3c5df67c941949
Akshay Zade <akshayzade707@gmail.com>
2024-05-01 02:05:58 +0530

    Dashboard update

--- employeeMS/src/component/Login.jsx
+++ employeeMS/src/component/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './style.css'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 
 const Login = () => {
@@ -8,12 +10,20 @@ const Login = () => {
         email:"",
         password:""
     })
+    const [error , setError] = useState(null)
+    const navigate = useNavigate()
+    axios.defaults.withCredentials = true ;
 
     const handleSubmit = (event) => {
        event.preventDefault();
        axios.post('http://localhost:5000/auth/adminlogin' , values)
        .then((result)=>{
-            console.log(result)
+        if(result.data.loginStatus){
+            navigate('/dashboard')
+        }else {
+            setError(result.data.Error)
+        }
+            
        })
        .catch((error)=>{
             console.log(error)
@@ -23,6 +33,9 @@ const Login = () => {
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm' >
+            <div className='text-warning' >
+                {error && error}
+            </div>
             <h2>Login Page</h2>
             <form onSubmit={handleSubmit} >
             <div className='mb-3' >
--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ import { adminRouter } from "./Routes/AdminRoute.js";
 const app = express();
 const PORT = 5000 ;
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    methods:['GET','POST','PUT'],
+    credentials:true
+
+}));
 app.use(express.json());
 app.use('/auth' , adminRouter)
 
